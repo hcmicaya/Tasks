41d2ba5e8e320262584c63310882e8f0f1379d66
Hans Micaya <hcmicaya@gmail.com>
2022-08-03 19:26:53 -0400

    Fix create on tasks reducer

--- client/src/reducers/tasks.js
+++ client/src/reducers/tasks.js
@@ -3,12 +3,12 @@ import { CREATE, UPDATE, DELETE } from "../constants/actionTypes";
 export default (state = { tasks: [] }, action) => {
     switch (action.type) {
         case CREATE:
-            return [...state, action.payload];
+            return [...state.tasks, action.payload];
         case DELETE:
             return {
                 ...state,
-                posts: state.posts.filter(
-                    (post) => post._id !== action.payload
+                tasks: state.tasks.filter(
+                    (task) => task._id !== action.payload
                 ),
             };
         default:
